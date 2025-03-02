96e8d8efd9c289078ee0016b8732e9c42e4ab796
_skaaalper <159729426+SKaaalper@users.noreply.github.com>
2025-03-02 17:45:01 +0800

    Update headers.js

--- headers.js
+++ headers.js
@@ -1,14 +1,17 @@
 const headers = {
-  "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+  "accept-language": "en-US,en;q=0.9",
   "cache-control": "no-cache",
   "content-type": "application/json",
   "pragma": "no-cache",
   "sec-ch-ua":
     '"Google Chrome";v="131", "Chromium";v="131", "Not-A.Brand";v="24"',
   "sec-ch-ua-mobile": "?0",
-  "sec-ch-ua-platform": '"macOS"',
+  "sec-ch-ua-platform": '"Linux"',
   "sec-fetch-dest": "empty",
   "sec-fetch-mode": "cors",
   "sec-fetch-site": "cross-site",
+  "user-agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
 };
+
 export default headers;
