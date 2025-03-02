f7751299b19c124e81e6db3bf6ab2ccccdae270f
_skaaalper <159729426+SKaaalper@users.noreply.github.com>
2025-03-02 16:57:47 +0800

    Create headers.js

---
+++ headers.js
@@ -0,0 +1,14 @@
+const headers = {
+  "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+  "cache-control": "no-cache",
+  "content-type": "application/json",
+  "pragma": "no-cache",
+  "sec-ch-ua":
+    '"Google Chrome";v="131", "Chromium";v="131", "Not-A.Brand";v="24"',
+  "sec-ch-ua-mobile": "?0",
+  "sec-ch-ua-platform": '"macOS"',
+  "sec-fetch-dest": "empty",
+  "sec-fetch-mode": "cors",
+  "sec-fetch-site": "cross-site",
+};
+export default headers;
